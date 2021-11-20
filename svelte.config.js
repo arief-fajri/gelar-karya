9b9fadc57360fec312ebfd8fb1d9b88c7c452ec2
arief-fajri <arief.fajri191@gmail.com>
2021-11-20 13:40:04 +0700

    try deploy to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,12 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
-
+import vercel from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
   kit: {
+    adapter: vercel(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
-    adapter: adapter({
-      pages: "build",
-      assets: "build",
-      fallback: null,
-    }),
-    paths: {
-      // YOUR github repository name
-      base: "/gelar-karya",
-    },
   },
 };
 
