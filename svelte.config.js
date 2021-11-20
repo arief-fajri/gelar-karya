23870632ede03a8128012b695062b2c64b09b396
arief-fajri <arief.fajri191@gmail.com>
2021-11-20 12:45:18 +0700

    fix deploy gh-page

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,10 @@ const config = {
       assets: "build",
       fallback: null,
     }),
+	paths: {
+      // YOUR github repository name
+      base: "/svelte-kit-github-page-example/",
+    },
   },
 };
 
